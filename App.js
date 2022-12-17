603534c5872f9980714f880dedfedb6dd42670d9
nishant4422 <120179233+nishant4422@users.noreply.github.com>
2022-12-17 18:07:16 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
-const codes = document.querySelectorAll(".code");
-
+const codes = document.querySelectorAll('.code');
 codes[0].focus();
 
 codes.forEach((code, idx) => {
-  code.addEventListener("keydown", (e) => {
-    if (e.key >= 0 && e.key <= 9) {
-      codes[idx].value = "";
-      setTimeout(() => codes[idx + 1].focus(), 10);
-    } else if (e.key === "Backspace") {
-      setTimeout(() => codes[idx - 1].focus(), 10);
-    }
-  });
-});
+    code.addEventListener('keydown', (e) => {
+        if(e.key >= 0 && e.key <= 9 ) {
+            codes[idx].value = '';
+            if(idx==5) return;
+            setTimeout(() => codes[idx +1].focus(),10)
+        } else if(e.key === 'Backspace' && idx!=0) {
+            if(idx<5){
+                setTimeout(() =>  codes[idx].value = '', codes[idx-1].focus(),10)
+            }
+            else if(idx==5 && codes[idx].value == ""){
+                setTimeout(() =>  codes[idx].value = '', codes[idx-1].focus(),10)
+            }
+            else{
+                setTimeout(() =>  codes[idx].focus(),10)
+            }
+        }
+    })
+})
