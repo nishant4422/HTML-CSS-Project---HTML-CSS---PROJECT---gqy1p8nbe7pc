23d7b6d864e51f734045b8246de0ebf7ddff8925
nishant4422 <120179233+nishant4422@users.noreply.github.com>
2022-12-17 16:11:31 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,60 +1,14 @@
-const inputs = document.querySelectorAll(".otp-input");
-const validInputValues = [
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "Backspace",
-];
+const codes = document.querySelectorAll(".code");
 
-const moveFocusToPreviousInput = (eventOriginationIndex) => {
-  if (eventOriginationIndex === 0) {
-    return;
-  }
+codes[0].focus();
 
-  const inputToFocus = inputs[eventOriginationIndex - 1];
-  inputToFocus.focus();
-};
-
-const moveFocusToNextInput = (eventOriginationIndex) => {
-  if (eventOriginationIndex === 5) {
-    return;
-  }
-
-  const inputToFocus = inputs[eventOriginationIndex + 1];
-  inputToFocus.focus();
-};
-
-const onInputChange = (event) => {
-  if (!validInputValues.includes(event.key)) {
-    return;
-  }
-
-  const input = event.target;
-  const index = parseInt(input.getAttribute("data-index"));
-
-  if (event.key === "Backspace") {
-    moveFocusToPreviousInput(index);
-    return;
-  }
-
-  moveFocusToNextInput(index);
-};
-
-inputs.forEach((input) => {
-  input.addEventListener("keyup", onInputChange);
-});
-
-inputs.forEach((input) => {
-  input.addEventListener("keydown", (event) => {
-    if (!validInputValues.includes(event.key)) {
-      event.preventDefault();
+codes.forEach((code, idx) => {
+  code.addEventListener("keydown", (e) => {
+    if (e.key >= 0 && e.key <= 9) {
+      codes[idx].value = "";
+      setTimeout(() => codes[idx + 1].focus(), 10);
+    } else if (e.key === "Backspace") {
+      setTimeout(() => codes[idx - 1].focus(), 10);
     }
   });
 });
